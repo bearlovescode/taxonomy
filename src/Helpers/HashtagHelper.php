81b8d00e687d881ff687904516afc1c81a44df2d
Nic Barr <code@nicholasbarr.net>
2024-05-15 21:29:11 -0500

    create hashtag helper to assist in extraction and conversion of a body of text.

--- src/Helpers/HashtagHelper.php
+++ src/Helpers/HashtagHelper.php
@@ -1,24 +1,34 @@
 <?php
-    namespace Bearlovescode\Taxonomy\Helpers;
+namespace Bearlovescode\Taxonomy\Helpers;
 
-    class HashtagHelper
+class HashtagHelper
+{
+    public static function extractHashtags(string $body, string $marker = '#'): array
     {
-        public static function extractHashtags(string $body, string $marker = '#'): array
-        {
-            $pattern = sprintf('/%s(\w+)/', $marker);
-            $matches = [];
+        $body = htmlspecialchars_decode($body, ENT_QUOTES | ENT_XML1);
+//            $body = (mb_convert_encoding($body, 'UTF-8', 'HTML-ENTITIES'));
+        $pattern = sprintf('/%s(\w+)/', $marker);
+        $matches = [];
 
-            preg_match_all($pattern, $body, $matches);
+        preg_match_all($pattern, $body, $matches);
 
-            return $matches;
-        }
-
-        public static function convertHashtagToLink(string $body, string $baseUrl): string
-        {
-            $hashtags = self::extractHashtags($body);
-
-            $updatedBody = $body;
-
-            return $updatedBody;
-        }
+        return $matches;
     }
+
+    public static function convertHashtagToLink(string $body, string $baseUrl): string
+    {
+        $hashtags = self::extractHashtags($body);
+
+        $updatedBody = $body;
+
+        foreach ($hashtags as $tag)
+        {
+            if (empty($tag) || empty($tag[0]))
+                continue;
+
+            $updatedBody = preg_replace(sprintf('/%s/', $tag[0]), sprintf('%s/%s', $baseUrl, $tag[0]), $updatedBody);
+        }
+
+        return $updatedBody;
+    }
+}
